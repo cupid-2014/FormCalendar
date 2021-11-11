eb90efcbc682e71db40930a17ed6528e0c809043
zhangzhen <zhangzhen@jugui-tech.com>
2021-11-11 11:35:57 +0800

    v1.0

---
+++ FormCalendar/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace FormCalendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.source = new System.Windows.Forms.GroupBox();
+            this.richTextBoxDescription = new System.Windows.Forms.RichTextBox();
+            this.DescriptionLabel = new System.Windows.Forms.Label();
+            this.textBoxSummary = new System.Windows.Forms.TextBox();
+            this.SUMMARYLabel = new System.Windows.Forms.Label();
+            this.dateTimePickerDTEnd = new System.Windows.Forms.DateTimePicker();
+            this.DTENDLabel = new System.Windows.Forms.Label();
+            this.dateTimePickerDTStart = new System.Windows.Forms.DateTimePicker();
+            this.DTSTARTLabel = new System.Windows.Forms.Label();
+            this.grid = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.source.SuspendLayout();
+            this.grid.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // source
+            // 
+            this.source.Controls.Add(this.richTextBoxDescription);
+            this.source.Controls.Add(this.DescriptionLabel);
+            this.source.Controls.Add(this.textBoxSummary);
+            this.source.Controls.Add(this.SUMMARYLabel);
+            this.source.Controls.Add(this.dateTimePickerDTEnd);
+            this.source.Controls.Add(this.DTENDLabel);
+            this.source.Controls.Add(this.dateTimePickerDTStart);
+            this.source.Controls.Add(this.DTSTARTLabel);
+            this.source.Location = new System.Drawing.Point(12, 12);
+            this.source.Name = "source";
+            this.source.Size = new System.Drawing.Size(200, 403);
+            this.source.TabIndex = 1;
+            this.source.TabStop = false;
+            this.source.Text = "Source";
+            // 
+            // richTextBoxDescription
+            // 
+            this.richTextBoxDescription.Location = new System.Drawing.Point(6, 279);
+            this.richTextBoxDescription.Name = "richTextBoxDescription";
+            this.richTextBoxDescription.Size = new System.Drawing.Size(188, 118);
+            this.richTextBoxDescription.TabIndex = 7;
+            this.richTextBoxDescription.Text = "";
+            // 
+            // DescriptionLabel
+            // 
+            this.DescriptionLabel.AutoSize = true;
+            this.DescriptionLabel.Location = new System.Drawing.Point(7, 248);
+            this.DescriptionLabel.Name = "DescriptionLabel";
+            this.DescriptionLabel.Size = new System.Drawing.Size(29, 12);
+            this.DescriptionLabel.TabIndex = 6;
+            this.DescriptionLabel.Text = "描述";
+            // 
+            // textBoxSummary
+            // 
+            this.textBoxSummary.Location = new System.Drawing.Point(6, 207);
+            this.textBoxSummary.Name = "textBoxSummary";
+            this.textBoxSummary.Size = new System.Drawing.Size(188, 21);
+            this.textBoxSummary.TabIndex = 5;
+            // 
+            // SUMMARYLabel
+            // 
+            this.SUMMARYLabel.AutoSize = true;
+            this.SUMMARYLabel.Location = new System.Drawing.Point(7, 183);
+            this.SUMMARYLabel.Name = "SUMMARYLabel";
+            this.SUMMARYLabel.Size = new System.Drawing.Size(29, 12);
+            this.SUMMARYLabel.TabIndex = 4;
+            this.SUMMARYLabel.Text = "标题";
+            // 
+            // dateTimePickerDTEnd
+            // 
+            this.dateTimePickerDTEnd.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dateTimePickerDTEnd.Location = new System.Drawing.Point(6, 138);
+            this.dateTimePickerDTEnd.Name = "dateTimePickerDTEnd";
+            this.dateTimePickerDTEnd.Size = new System.Drawing.Size(188, 21);
+            this.dateTimePickerDTEnd.TabIndex = 3;
+            // 
+            // DTENDLabel
+            // 
+            this.DTENDLabel.AutoSize = true;
+            this.DTENDLabel.Location = new System.Drawing.Point(7, 107);
+            this.DTENDLabel.Name = "DTENDLabel";
+            this.DTENDLabel.Size = new System.Drawing.Size(53, 12);
+            this.DTENDLabel.TabIndex = 2;
+            this.DTENDLabel.Text = "结束日期";
+            // 
+            // dateTimePickerDTStart
+            // 
+            this.dateTimePickerDTStart.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dateTimePickerDTStart.Location = new System.Drawing.Point(6, 65);
+            this.dateTimePickerDTStart.Name = "dateTimePickerDTStart";
+            this.dateTimePickerDTStart.Size = new System.Drawing.Size(188, 21);
+            this.dateTimePickerDTStart.TabIndex = 1;
+            // 
+            // DTSTARTLabel
+            // 
+            this.DTSTARTLabel.AutoSize = true;
+            this.DTSTARTLabel.Location = new System.Drawing.Point(7, 38);
+            this.DTSTARTLabel.Name = "DTSTARTLabel";
+            this.DTSTARTLabel.Size = new System.Drawing.Size(53, 12);
+            this.DTSTARTLabel.TabIndex = 0;
+            this.DTSTARTLabel.Text = "开始日期";
+            // 
+            // grid
+            // 
+            this.grid.Controls.Add(this.dataGridView1);
+            this.grid.Location = new System.Drawing.Point(218, 12);
+            this.grid.Name = "grid";
+            this.grid.Size = new System.Drawing.Size(401, 403);
+            this.grid.TabIndex = 2;
+            this.grid.TabStop = false;
+            this.grid.Text = "Grid";
+            // 
+            // dataGridView1
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 17);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(395, 383);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellMouseDown += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseDown);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "原始数据";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(70, 421);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "暂存";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(385, 421);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "生成";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
+            this.contextMenuStrip1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.contextMenuStrip1_MouseClick);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem1.Text = "删除行";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem2.Text = "导入（会覆盖）";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.grid);
+            this.Controls.Add(this.source);
+            this.Name = "Form1";
+            this.Text = "日历制作.ics";
+            this.source.ResumeLayout(false);
+            this.source.PerformLayout();
+            this.grid.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox source;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDTStart;
+        private System.Windows.Forms.Label DTSTARTLabel;
+        private System.Windows.Forms.GroupBox grid;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label DTENDLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBoxDescription;
+        private System.Windows.Forms.Label DescriptionLabel;
+        private System.Windows.Forms.TextBox textBoxSummary;
+        private System.Windows.Forms.Label SUMMARYLabel;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDTEnd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+    }
+}
+
---
+++ FormCalendar/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormCalendar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 暂存数据到gridview
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder("");
+            sb.Append("BEGIN:VEVENT").Append("\n");
+
+            // 开始日期
+            //String DTStart = dateTimePickerDTStart.Value.ToString("yyyyMMddTHHmmssZ");
+            String DTStart = dateTimePickerDTStart.Value.ToString("yyyyMMdd");
+            sb.Append("DTSTART:").Append(DTStart).Append("\n");
+            // 开始日期
+            //String DTEnd = dateTimePickerDTEnd.Value.ToString("yyyyMMddTHHmmssZ");
+            String DTEnd = dateTimePickerDTEnd.Value.ToString("yyyyMMdd");
+            sb.Append("DTEND:").Append(DTEnd).Append("\n");
+            // 标题
+            String Summary = textBoxSummary.Text;
+            if (String.IsNullOrEmpty(Summary))
+            {
+                MessageBox.Show("【标题】不能为空");
+                return;
+            }
+            sb.Append("SUMMARY:").Append(Summary).Append("\n");
+            // 描述
+            String Description = richTextBoxDescription.Text;
+            if (!string.IsNullOrEmpty(Description))
+            {
+                sb.Append("DESCRIPTION:").Append(Description).Append("\n");
+            }
+            // uid
+            String uid = System.Guid.NewGuid().ToString();
+            sb.Append("UID:").Append(uid).Append("\n");
+
+            sb.Append("END:VEVENT").Append("\n");
+
+
+            int index = this.dataGridView1.Rows.Add();
+            this.dataGridView1.Rows[index].Cells[0].Value = sb.ToString();
+
+
+        }
+
+        /// <summary>
+        /// 生成
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path = String.Empty;
+            System.Windows.Forms.FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                path = fbd.SelectedPath;
+            }
+
+            // 如果未选择目录则不导出
+            if (string.IsNullOrEmpty(path))
+                return;
+
+            StringBuilder sb = new StringBuilder("");
+            sb.Append("BEGIN:VCALENDAR").Append("\n");
+            sb.Append("PRODID:-//z-f.top//calendar 2.2//EN").Append("\n");
+            sb.Append("VERSION:2.0").Append("\n");
+            sb.Append("CALSCALE:GREGORIAN").Append("\n");
+
+            int rows = this.dataGridView1.Rows.Count - 1;
+            for (int i = 0; i < rows; i++)
+            {
+                sb.Append("").Append(this.dataGridView1.Rows[i].Cells[0].Value.ToString());
+            }
+
+            sb.Append("END:VCALENDAR").Append("\n");
+
+            StreamWriter sw = new StreamWriter(path + "iCalendar.ics");
+            sw.WriteLine(sb.ToString());
+            sw.Close();
+            MessageBox.Show("文件成功输出到【" + path + "iCalendar.ics】");
+        }
+
+
+        private int indexCon = 0;
+
+        /// <summary>
+        /// grid 部分窗体右键控制
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dataGridView1_CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                if (e.RowIndex >= 0)
+                {
+                    this.dataGridView1.Rows[e.RowIndex].Selected = true;
+                    indexCon = e.RowIndex;
+                    this.dataGridView1.CurrentCell = this.dataGridView1.Rows[e.RowIndex].Cells[0];
+
+                    this.contextMenuStrip1.Show(this.dataGridView1, e.Location);
+                    this.contextMenuStrip1.Show(Cursor.Position);
+                }
+            }
+        }
+
+        private void contextMenuStrip1_MouseClick(object sender, MouseEventArgs e)
+        {
+            foreach (ToolStripMenuItem t in contextMenuStrip1.Items)
+            {
+                if (t.Selected == true)
+                {
+                    if (t.Name == this.toolStripMenuItem1.Name)
+                    {
+                        // 删除
+                        if (this.dataGridView1.Rows.Count - 1 == indexCon)
+                        {
+                            MessageBox.Show("未提交的行无法删除！");
+                            return;
+                        }
+                        // 删除行
+                        this.dataGridView1.Rows.RemoveAt(indexCon);
+                    }
+                    else if (t.Name == this.toolStripMenuItem2.Name)
+                    {
+                        // 隐藏邮件菜单
+                        this.contextMenuStrip1.Hide();
+                        // 导入
+                        System.Windows.Forms.OpenFileDialog fbd = new OpenFileDialog();
+                        if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                        {
+                            StreamReader sr = new StreamReader(fbd.FileName);
+                            String strLine = String.Empty;
+                            StringBuilder sb = new StringBuilder("");
+                            List<String> resultList = new List<string>();
+                            bool flag = false;
+
+                            // 读取附件数据
+                            strLine = sr.ReadLine();
+                            while (strLine != null)
+                            {
+                                if (strLine.Equals("BEGIN:VEVENT") || flag)
+                                {
+                                    flag = true;
+                                    sb.Append(strLine).Append("\n");
+                                }
+                                if (strLine.Equals("END:VEVENT"))
+                                {
+                                    flag = false;
+                                    resultList.Add(sb.ToString());
+                                    sb = new StringBuilder("");
+                                }
+                                strLine = sr.ReadLine();
+                            }
+
+                            // 清空grid
+                            int rc = this.dataGridView1.RowCount - 1; // 下表从0开始，所以-1
+                            for (int i = rc - 1; i >= 0; i--) // 最后一个不删除，所以-1
+                            { 
+                                // 删除行
+                                this.dataGridView1.Rows.RemoveAt(i);
+                            }
+                            // 加入grid
+                            foreach (String item in resultList)
+                            {
+                                int index = this.dataGridView1.Rows.Add();
+                                this.dataGridView1.Rows[index].Cells[0].Value = item;
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
